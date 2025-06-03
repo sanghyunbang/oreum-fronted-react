5c73e8e3f68247e9f644e363c531dc22af263609
gaa149 <gaa5e9e@gmail.com>
2025-06-03 12:13:44 +0900

    feat: 로그인 기능구현

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LoginPage = ()=>{
+    const [formData, setFormData] = useState({userid:"",pw:""});
+    const people = {userid:"안녕",pw:"123"};
+    const doChange = (e)=>{
+        setFormData(prev=>({...prev,[e.target.name]:e.target.value}));
+        console.log(formData.userid);
+    }
+    const doSubmit=async(e)=>{
+         e.preventDefault();
+        if(formData.userid===""){
+            alert("올바른 아이디(을)를 입력하시오.");
+            return;
+        }else if(formData.pw===""){
+            alert("올바른 비밀번호(을)를 입력하시오.");
+            return;
+        }
+        if(formData.userid !== people.userid || formData.pw !== people.pw){
+            alert("아이디 또는 비밀번호가 일치하지 않습니다.");
+            return;
+        }else{
+            alert("로그인 성공!!!");
+        }
+    }
     return (
-        <div>
-            <h2>로그인</h2>
+        <div style={{textAlign:'center'}}>
+            <h1>로그인</h1>
+            <div>
+                <form onSubmit={doSubmit}>
+                    아이디<input type='text' name='userid' value={formData.userid||""} onChange={doChange} /><br/><br/>
+                    비밀번호<input type='text' name='pw' value={formData.pw||""} onChange={doChange} /> <br/><br/>
+                    <button type='submit'>로그인</button> <br/>아이디 찾기 비밀번호찾기 회원가입
+                </form>
+            </div>
         </div>
     )
 }
