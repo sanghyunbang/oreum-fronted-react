c4bf03a02f56ea43798c2ab98401ae5ddd2dc44d
HYUN <149238724+sanghyunbang@users.noreply.github.com>
2025-06-06 01:17:51 +0900

    favicon 추가

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
     <header className="bg-green-700 text-white px-6 py-3 flex justify-between items-center shadow">
-      <Link to="/" className="text-xl font-bold">오름 | OREUM ⛰</Link>
+      <Link to="/" className="flex items-center text-xl font-bold gap-2">
+        <img
+          src={`${process.env.PUBLIC_URL}/favicon.png`}
+          alt="오름 로고"
+          className="w-8 h-8"
+        />
+        오름 | OREUM
+      </Link>
+
       <div className="flex items-center gap-4">
         <input
           type="text"
@@ -16,6 +25,5 @@ const Header = () => {
     </header>
   );
 };
-
 
 export default Header;
