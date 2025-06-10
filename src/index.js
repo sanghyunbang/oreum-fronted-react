f9f7d096dbec46e06b705607da42e5594de80554
HYUN <149238724+sanghyunbang@users.noreply.github.com>
2025-06-10 23:09:17 +0900

    fix: strict모드 제거

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import { store } from './redux/store'; // ✅ store 경로 확인!
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
